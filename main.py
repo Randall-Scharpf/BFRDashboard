541a9a71d3958b1a429e54000a14f54960644d91
Jason Liu <111265664+liug26@users.noreply.github.com>
2023-02-16 22:21:53 -0800

    Tried uploading gui to RP

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ import resources  # https://www.pythonguis.com/tutorials/qresource-system/
 import update
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtCore import QFile, QTimer, QDateTime
+from PyQt5.QtCore import Qt, QFile, QTimer, QDateTime, QRunnable, QThreadPool
 
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         QMainWindow.__init__(self)
+        # self.setWindowFlags(Qt.FramelessWindowHint)
         main_ui = QFile(":/res/main_ui")
         main_ui.open(QFile.ReadOnly)
         # run this command: pyrcc5 resources.qrc -o resources.py to package main_ui
@@ -22,10 +23,13 @@ class MainWindow(QMainWindow):
         timer = QTimer(self)
         timer.timeout.connect(update.on_update_labels)
         timer.start(update.ms_per_update)
+        self.show()
+        # self.showMaximized()  # 1470 920
+        print("Current screen width: " + str(self.frameGeometry().width()) + ", height: " + str(self.frameGeometry().height()))
 
 
 if __name__ == "__main__":
+    QApplication.setStyle("fusion")
     app = QApplication([])
     window = MainWindow()
-    window.show()
     sys.exit(app.exec_())
