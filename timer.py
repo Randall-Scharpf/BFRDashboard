399999b904daeb7b0092d49791c3275b2df4758c
Jason Liu <111265664+liug26@users.noreply.github.com>
2023-02-22 00:14:19 -0800

    Update small things and added logging

--- timer.py
+++ timer.py
@@ -1,22 +1,23 @@
 import globalfonts
+import logging
 from PyQt5.QtCore import QDateTime
 
 
 # constants
+MS_PER_UPDATE = 10
 ELAPSED_UPDATE_TOLERANCE = 1000
 ELAPSED_UPDATE_MAX = 99 * 1000
-# parameters to initialize before calling update
+# to be initialized before calling on_update_labels
 main_win = None
-ms_per_update = -1
 
 elapsed_ms = 1000
 elapsed_update_time = ELAPSED_UPDATE_TOLERANCE
 
 
 def on_update_labels():
-    global main_win, ms_per_update, elapsed_ms, elapsed_update_time
-    elapsed_ms += ms_per_update
-    elapsed_update_time += ms_per_update
+    global MS_PER_UPDATE, main_win, elapsed_ms, elapsed_update_time
+    elapsed_ms += MS_PER_UPDATE
+    elapsed_update_time += MS_PER_UPDATE
 
     # update Time Label every whole second
     if elapsed_ms > 1000:
@@ -35,6 +36,7 @@ def on_update_labels():
     else:
         main_win.CANConnectionLabel.setText("Connected")
         main_win.CANStatusLabel.setStyleSheet(globalfonts.FONT_CSS + "color: green;"  + globalfonts.TRANSPARENT_CSS + globalfonts.scaled_css_size(25))
+
 
 def on_update_data():
     global elasped_update_time
